be231a9c3386a445b88e88e000f91956db28507d
Radu Vadim <raduvadim@gmail.com>
2015-04-02 14:29:48 +0300

    Update dsp_drv.vhd
    initial structure and definitions

--- 7seg/dsp_drv.vhd
+++ 7seg/dsp_drv.vhd
@@ -1,1 +1,11 @@
 --driver pentru afisajul cu sapte segmente
+entity dsp_drv is
+  port( clk: in std_logic;
+        rst: in std_logic;
+        data_in: in std_logic_vector( 15 downto 0);
+        an: out std_logic_vector(3 downto 0);
+        c: out std_logic_vector(7 downto 0));
+end entity;
+architecture Behavioral of dsp_drv is
+begin
+end architecture;
